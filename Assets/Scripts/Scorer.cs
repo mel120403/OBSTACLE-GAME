66beff270288b55103914434d90243c250c6c356
mel120403 <melwilapr12@gmail.com>
2023-09-14 20:35:52 +0530

    Commit

---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    int hits=0;
+   private void OnCollisionEnter(Collision other) 
+   {
+        if(other.gameObject.tag!="hit")
+        {
+            hits++;
+         Debug.Log("Number of times you hit the wall is:"+hits);
+        }
+        
+   }
+}
